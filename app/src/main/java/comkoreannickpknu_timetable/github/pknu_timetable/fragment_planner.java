ae97f2362c7402fdfeea5634e450110ad7571b0f
koreannick <fpahsk3238>
2017-02-12 03:59:25 +0900

    fragment 추가

---
+++ app/src/main/java/comkoreannickpknu_timetable/github/pknu_timetable/fragment_planner.java
@@ -0,0 +1,18 @@
+package comkoreannickpknu_timetable.github.pknu_timetable;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by EunYeong on 2017-02-11.
+ */
+
+public class fragment_planner extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
+        return inflater.inflate(R.layout.fragment_planner,container,false);
+    }
+
+}
---
+++ app/src/main/java/comkoreannickpknu_timetable/github/pknu_timetable/fragment_schedule.java
@@ -0,0 +1,17 @@
+package comkoreannickpknu_timetable.github.pknu_timetable;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by EunYeong on 2017-02-11.
+ */
+
+public class fragment_schedule extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
+        return inflater.inflate(R.layout.fragment_schedule,container,false);
+    }
+}
